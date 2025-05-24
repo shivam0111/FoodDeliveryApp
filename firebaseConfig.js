6429fe5c3633d9e182b1b38dde2d97d454f925b5
shivam0111 <devaarora100@gmail.com>
2025-05-24 13:49:48 +0530

    third

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,14 +1,6 @@
 import { initializeApp } from '@react-native-firebase/app';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyB9IDjDl0NnRS3vdX61lovv2Hu0Rc8Fxjw",
-  authDomain: "fooddelivery.firebaseapp.com",
-  projectId: "fooddeliveryapp-5af3d",
-  storageBucket: "fooddeliveryapp-5af3d.firebasestorage.app",
-  messagingSenderId: "9718244683",
-  appId: "1:754104202668:android:46ae945f01d1de2fc47609",
-};
 
-const app = initializeApp(firebaseConfig);
+const app = initializeApp();
 
 export default app;
